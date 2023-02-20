b38ead11a1ef46fbdec27084ff65f9f6a2ee88c7
Res-lay <82407533+Res-lay@users.noreply.github.com>
2023-02-20 22:25:05 +0300

    first commit

---
+++ practices/src/main/java/pr1.java
@@ -0,0 +1,18 @@
+import java.rmi.MarshalException;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.function.Consumer;
+
+public class pr1 {
+    public static void main(String[] args) {
+
+        ////////////////////////////////////////////////////////
+        Consumer<int[]> sort = mass -> {Arrays.sort(mass);
+            System.out.println(Arrays.toString(mass));};
+        int[] a = {3,1,10,5,2,4};
+        sort.accept(a);
+        ////////////////////////////////////////////////////////
+
+
+    }
+}
